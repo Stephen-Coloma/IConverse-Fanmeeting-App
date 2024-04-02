ad22d8a8345e0a66c0af208c08a596ae178ecbd3
MARIUS GLENN NONATO <2232731@slu.edu.ph>
2024-04-02 20:07:23 +0800

    feat: Added Reference classes for Users and Fan Meets
    >This commit adds two reference classes named User and Fanmeet

--- src/main/java/shared/Fanmeet.java
+++ src/main/java/shared/Fanmeet.java
@@ -1,4 +1,79 @@
 package shared;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Fanmeet {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("h:mm a");
+    private int fanMeetID;
+    private User idolName; // Reference to User class
+    private LocalDate date;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private double pricePerMinute;
+
+    // Constructor
+    public Fanmeet(int fanMeetID, User idolName, LocalDate date, LocalTime startTime, LocalTime endTime, double pricePerMinute) {
+        this.fanMeetID = fanMeetID;
+        this.idolName = idolName;
+        this.date = date;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.pricePerMinute = pricePerMinute;
+    }
+
+    // Getters and setters
+    public int getFanMeetID() {
+        return fanMeetID;
+    }
+
+    public void setFanMeetID(int fanMeetID) {
+        this.fanMeetID = fanMeetID;
+    }
+    public User getIdolName(){
+        return idolName;
+    }
+
+    public void setIdolName(User user) {
+        this.idolName = user;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public double getPricePerMinute() {
+        return pricePerMinute;
+    }
+
+    public void setPricePerMinute(double pricePerMinute) {
+        this.pricePerMinute = pricePerMinute;
+    }
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getFormattedStartTime(){
+        return startTime.format(formatter);
+    }
+    public String getFormattedEndTime(){
+        return endTime.format(formatter);
+    }
 }
